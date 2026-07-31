564b2a585d407b17a0eeed38854bd7f8a6233ac7
Xiaoke Zhu <zhuxiaoke007@163.com>
2026-07-31 15:20:11 +0800

    修复第19行语法错误：add_contact(contacts person) -> add_contact(contacts, person)，缺少逗号导致参数列表解析失败

--- lesson01/main.py
+++ lesson01/main.py
@@ -1,45 +1,45 @@
 contacts = []
 
 def show_menu():
-    print("1 瘛餃??頂鈭?)
-    print("2 ?亦??頂鈭?)
-    print("3 ??頂鈭?)
-    print("0 ???)
+    print("1 添加联系人")
+    print("2 查看联系人")
+    print("3 删除联系人")
+    print("0 退出")
 
 def get_choice():
     choice = input()
     return choice
 
 def input_contact():
-    name = input("憪?嚗?)
-    phone = input("?菔?嚗?)
+    name = input("姓名：")
+    phone = input("电话：")
     person = [name, phone]
     return person
 
-def add_contact(contacts, person):
+def add_contact(contacts person):
     contacts.append(person)
 
 def show_contacts(contacts):
     if not contacts:
-        print("?悖敶蛹蝛?)
+        print("通讯录为空")
         return
     for person in contacts:
-        print("憪?嚗?, person[0])
-        print("?菔?嚗?, person[1])
+        print("姓名：", person[0])
+        print("电话：", person[1])
         print("----------------")
 
 def delete_contact(contacts):
     if not contacts:
-        print("?悖敶蛹蝛?)
+        print("通讯录为空")
         return
     show_contacts(contacts)
-    number = input("霂瑁??亦??瘀?") 
+    number = input("请输入编号：") 
     number = int(number)
     if 1 <= number <= len(contacts):
         contacts.pop(number - 1)
-        print("???")
+        print("删除成功")
     else:
-        print("蝻銝???)
+        print("编号不存在")
 
 
 while True:
@@ -57,4 +57,4 @@ while True:
     elif choice == "3":
         delete_contact(contacts)
 
-print("??箇?摨?)
+print("退出程序")
