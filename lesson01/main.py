0940a4f23a890e8981b0435564aa3b0241bfc151
Xiaoke Zhu <zhuxiaoke007@163.com>
2026-07-31 15:14:26 +0800

    ???19?????:add_contact(contacts person) -> add_contact(contacts, person),??????????????

--- lesson01/main.py
+++ lesson01/main.py
@@ -1,45 +1,45 @@
 contacts = []
 
 def show_menu():
-    print("1 添加联系人")
-    print("2 查看联系人")
-    print("3 删除联系人")
-    print("0 退出")
+    print("1 瘛餃??頂鈭?)
+    print("2 ?亦??頂鈭?)
+    print("3 ??頂鈭?)
+    print("0 ???)
 
 def get_choice():
     choice = input()
     return choice
 
 def input_contact():
-    name = input("姓名：")
-    phone = input("电话：")
+    name = input("憪?嚗?)
+    phone = input("?菔?嚗?)
     person = [name, phone]
     return person
 
-def add_contact(contacts person):
+def add_contact(contacts, person):
     contacts.append(person)
 
 def show_contacts(contacts):
     if not contacts:
-        print("通讯录为空")
+        print("?悖敶蛹蝛?)
         return
     for person in contacts:
-        print("姓名：", person[0])
-        print("电话：", person[1])
+        print("憪?嚗?, person[0])
+        print("?菔?嚗?, person[1])
         print("----------------")
 
 def delete_contact(contacts):
     if not contacts:
-        print("通讯录为空")
+        print("?悖敶蛹蝛?)
         return
     show_contacts(contacts)
-    number = input("请输入编号：") 
+    number = input("霂瑁??亦??瘀?") 
     number = int(number)
     if 1 <= number <= len(contacts):
         contacts.pop(number - 1)
-        print("删除成功")
+        print("???")
     else:
-        print("编号不存在")
+        print("蝻銝???)
 
 
 while True:
@@ -57,4 +57,4 @@ while True:
     elif choice == "3":
         delete_contact(contacts)
 
-print("退出程序")
+print("??箇?摨?)
